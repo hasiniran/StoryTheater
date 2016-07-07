4d3db79b513fb9e5dc7ba0622f727154a493d401
hasiniran <Randy@Hasinis-MacBook-Air.local>
2016-07-07 01:48:03 -0400

    Preparing old project for move

--- Autism App/ScreenCapture.m
+++
@@ -1,9 +0,0 @@
-//
-//  ScreenCapture.m
-//  Autism App
-//
-//  Created by Ben Kennel on 6/9/16.
-//  Copyright © 2016 Mobile Computing Lab. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
